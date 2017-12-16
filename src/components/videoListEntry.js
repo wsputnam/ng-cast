7df722f1649f30bf6491d67fb9e7f12188a7f655
wsputnam <williamputnamjr@gmail.com>
2017-12-15 17:30:16 -0800

    got video player linked to first video

--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -2,13 +2,13 @@ angular.module('video-player')
 .component('videoListEntry', {
   // TODO
   bindings: {
-    video: '<',
-    videoUrl: '<'
+    video: '<'
   },
   controller: function() {
     this.videoTitle = window.exampleVideoData[0].snippet.title;
     this.videoDescription = window.exampleVideoData[0].snippet.description;
-    this.videoUrl = 'https://www.youtube.com/embed/' + window.exampleVideoData[0].id.videoId;
+    this.videoUrl = 'https://www.youtube.com/embed/' + window.exampleVideoData[0].id.videoId; 
+    this.videoThumbnail = window.exampleVideoData[0].snippet.thumbnails.default.url;
   },
 
   templateUrl: 'src/templates/videoListEntry.html'
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -2,9 +2,14 @@ angular.module('video-player')
 
 .component('videoPlayer', {
   // TODO
+  bindings: {
+    video: '<'
+  },
   controller: function() {
-    this.data = window.exampleVideoData,
-    this.playerUrl = 'https://www.youtube.com/embed/' + window.exampleVideoData[0].id.videoId;
+    this.videoTitle = window.exampleVideoData[0].snippet.title;
+    this.videoDescription = window.exampleVideoData[0].snippet.description;
+    this.videoUrl = 'https://www.youtube.com/embed/' + window.exampleVideoData[0].id.videoId; 
+    this.videoThumbnail = window.exampleVideoData[0].snippet.thumbnails.default.url;
   },
   templateUrl: 'src/templates/videoPlayer.html'
 });
