3992afb2786e38ad16ce11ac3291587f2ae2b6bd
wsputnam <williamputnamjr@gmail.com>
2017-12-15 21:29:34 -0800

    attempted ajax call

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,19 @@
 angular.module('video-player')
-.service('youTube', function(){
-  // TODO
+.service('youTube', function() {
+  // Simple GET request example:
+  $http({
+    method: 'GET',
+    url: '/someUrl',
+    q: 'query',
+    dataType: 'json',
+    maxResults: 5,
+    key: 'youtube key',
+    videoEmbeddable: true
+  }).then(function successCallback(response, callback) {
+    console.log('success');
+    callback(response.items);
+  }, function errorCallback(response) {
+    console.log('ERROR!!!');
+  });
+
 });
